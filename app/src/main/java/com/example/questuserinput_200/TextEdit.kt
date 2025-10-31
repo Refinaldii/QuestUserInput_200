79faba49c26b222b7f3d7e578f79c03e7da7d8fa
Refinaldii <refinaldi9786@gmail.com>
2025-10-31 10:30:13 +0700

    Tambahkan ElevatedCard untuk menampilkan hasil input pengguna

--- app/src/main/java/com/example/questuserinput_200/TextEdit.kt
+++ app/src/main/java/com/example/questuserinput_200/TextEdit.kt
@@ -88,3 +88,18 @@ modifier = Modifier.padding(vertical = 8.dp),
 thickness = 1.dp,
 color = Color.DarkGray
 )
+ElevatedCard(
+elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+colors = CardDefaults.cardColors(containerColor = Color.Black),
+modifier = Modifier
+.height(100.dp)
+.width(300.dp)
+) {
+    Column(
+        modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp)
+    ) {
+        Text(text = "Nama    : $nama", color = Color.White)
+        Text(text = "Gender  : $jenis", color = Color.White)
+        Text(text = "Alamat  : $alamat", color = Color.White)
+    }
+}
